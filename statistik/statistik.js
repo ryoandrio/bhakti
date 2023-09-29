a517108c231d5c6aaefdec022b78571b6bd39f83
ryoandrio <ryoandrio06@gmail.com>
2023-09-29 18:46:18 +0700

    Map Setting 2nd

--- statistik/statistik.js
+++ statistik/statistik.js
@@ -37,67 +37,6 @@ async function loaddata() {
     if (datashow == 1) {
         console.log(json[0].title);
 
-        // var options = {
-        //     series: [
-        //         {
-        //             name: json[0].title,
-        //             data: json[0].data,
-        //         },
-        //     ],
-        //     chart: {
-        //         type: "bar",
-        //         height: 700,
-        //     },
-        //     plotOptions: {
-        //         bar: {
-        //             borderRadius: 4,
-        //             distributed: true,
-        //             horizontal: true,
-        //             dataLabels: {
-        //                 position: "top",
-        //             },
-        //         },
-        //     },
-        //     xaxis: {
-        //         categories: json[0].categories,
-        //     },
-        //     yaxis: {
-        //         title: {
-        //             text: "Kecamatan",
-        //             style: {
-        //                 fontSize: "14px",
-        //             },
-        //         },
-        //     },
-        //     colors: json[0].colors,
-        //     tooltip: {
-        //         y: {
-        //             formatter: function (val) {
-        //                 return val + " Jiwa";
-        //             },
-        //         },
-        //     },
-        //     title: {
-        //         text: json[0].title,
-        //         align: "center",
-        //     },
-        //     dataLabels: {
-        //         enabled: true,
-        //         style: {
-        //             fontSize: "12px",
-        //             colors: ["#000"],
-        //         },
-        //         offsetX: -5,
-        //     },
-        //     legend: {
-        //         position: "top",
-        //     },
-        // };
-
-        // var chart = new ApexCharts(document.querySelector("#chart"), options);
-        // $("#loading").remove();
-        // chart.render();
-
         var options = {
             series: [
                 {
@@ -106,60 +45,58 @@ async function loaddata() {
                 },
             ],
             chart: {
-                height: 350,
-                type: "line",
-                dropShadow: {
-                    enabled: true,
-                    color: "#000",
-                    top: 18,
-                    left: 7,
-                    blur: 10,
-                    opacity: 0.2,
+                type: "bar",
+                height: 700,
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 4,
+                    distributed: true,
+                    horizontal: true,
+                    dataLabels: {
+                        position: "top",
+                    },
                 },
-                toolbar: {
-                    show: false,
-                },
-            },
-            colors: json[0].colors,
-            dataLabels: {
-                enabled: true,
-            },
-            stroke: {
-                curve: "smooth",
-            },
-            title: {
-                text: json[0].title,
-                align: "left",
-            },
-            grid: {
-                borderColor: "#e7e7e7",
-                row: {
-                    colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-                    opacity: 0.5,
-                },
-            },
-            markers: {
-                size: 1,
             },
             xaxis: {
                 categories: json[0].categories,
                 title: {
-                    text: "Month",
+                    text: "Jumlah Tim",
+                    style: {
+                        fontSize: "14px",
+                    },
                 },
             },
             yaxis: {
                 title: {
-                    text: "Jumlah Tim",
+                    text: "Provinsi",
+                    style: {
+                        fontSize: "14px",
+                    },
                 },
-                // min: 5,
-                // max: 40,
+            },
+            colors: json[0].colors,
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return val + " Tim";
+                    },
+                },
+            },
+            title: {
+                text: json[0].title,
+                align: "center",
+            },
+            dataLabels: {
+                enabled: true,
+                style: {
+                    fontSize: "12px",
+                    colors: ["#000"],
+                },
+                offsetX: -5,
             },
             legend: {
-                position: "top",
-                horizontalAlign: "right",
-                floating: true,
-                offsetY: -25,
-                offsetX: -5,
+                enabled: false,
             },
         };
 
@@ -175,18 +112,15 @@ async function loaddata() {
                     name: json[1].title,
                     data: json[1].data,
                 },
-                {
-                    name: json[2].title,
-                    data: json[2].data,
-                },
             ],
             chart: {
                 type: "bar",
-                height: 1400,
+                height: 700,
             },
             plotOptions: {
                 bar: {
                     borderRadius: 4,
+                    distributed: true,
                     horizontal: true,
                     dataLabels: {
                         position: "top",
@@ -195,25 +129,31 @@ async function loaddata() {
             },
             xaxis: {
                 categories: json[1].categories,
-            },
-            yaxis: {
                 title: {
-                    text: "Kecamatan",
+                    text: "Jumlah Mahasiswa",
                     style: {
                         fontSize: "14px",
                     },
                 },
             },
-            colors: ["#548CFF", "#D96098"],
+            yaxis: {
+                title: {
+                    text: "Jumlah Mahasiswa",
+                    style: {
+                        fontSize: "14px",
+                    },
+                },
+            },
+            colors: json[1].colors,
             tooltip: {
                 y: {
                     formatter: function (val) {
-                        return val + " Jiwa";
+                        return val + " Mahasiswa";
                     },
                 },
             },
             title: {
-                text: "Jumlah Penduduk Menurut Jenis Kelamin",
+                text: json[1].title,
                 align: "center",
             },
             dataLabels: {
@@ -224,253 +164,9 @@ async function loaddata() {
                 },
                 offsetX: -5,
             },
-            legend: {
-                position: "top",
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        $("#loading").remove();
-        chart.render();
-    } else if (datashow == 3) {
-        console.log(json[4].title);
-
-        var options = {
-            series: [
-                {
-                    name: json[4].title,
-                    data: json[4].data,
-                },
-                {
-                    name: json[5].title,
-                    data: json[5].data,
-                },
-            ],
-            chart: {
-                type: "bar",
-                height: 700,
-                stacked: true,
-                stackType: "100%",
-            },
-            plotOptions: {
-                bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                    dataLabels: {
-                        position: "top",
-                    },
-                },
-            },
-            xaxis: {
-                categories: json[4].categories,
-            },
-            yaxis: {
-                title: {
-                    text: "Kelompok Umur",
-                    style: {
-                        fontSize: "14px",
-                    },
-                },
-            },
-            colors: ["#548CFF", "#D96098"],
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return val + " Jiwa";
-                    },
-                },
-            },
-            title: {
-                text: "Jumlah Penduduk Menurut Kelompok Umur",
-                align: "center",
-            },
-            dataLabels: {
-                enabled: true,
-                style: {
-                    fontSize: "12px",
-                    colors: ["#000"],
-                },
-                offsetX: -5,
-            },
-            legend: {
-                position: "top",
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        $("#loading").remove();
-        chart.render();
-    } else if (datashow == 4) {
-        console.log(json[3].title);
-
-        var options = {
-            series: json[3].data,
-            chart: {
-                width: 600,
-                type: "donut",
-                toolbar: {
-                    show: true,
-                },
-            },
-            labels: json[3].categories,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 400,
-                        },
-                    },
-                },
-            ],
-            plotOptions: {
-                pie: {
-                    donut: {
-                        labels: {
-                            show: true,
-                            value: {},
-                            total: {
-                                show: true,
-                            },
-                        },
-                    },
-                },
-            },
-            dataLabels: {
-                enabled: true,
-                style: {
-                    fontSize: "12px",
-                    colors: ["#fff"],
-                },
-            },
-            colors: json[3].colors,
-            title: {
-                text: json[3].title,
-                align: "center",
-            },
-            legend: {
-                position: "top",
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        $("#loading").remove();
-        chart.render();
-    } else if (datashow == 5) {
-        console.log(json[6].title);
-
-        var options = {
-            series: json[6].data,
-            chart: {
-                width: 600,
-                type: "donut",
-                toolbar: {
-                    show: true,
-                },
-            },
-            labels: json[6].categories,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 400,
-                        },
-                    },
-                },
-            ],
-            plotOptions: {
-                pie: {
-                    donut: {
-                        labels: {
-                            show: true,
-                            value: {},
-                            total: {
-                                show: true,
-                            },
-                        },
-                    },
-                },
-            },
-            dataLabels: {
-                enabled: true,
-                style: {
-                    fontSize: "12px",
-                    colors: ["#fff"],
-                },
-            },
-            colors: json[6].colors,
-            title: {
-                text: json[6].title,
-                align: "center",
-            },
-            legend: {
-                position: "top",
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        $("#loading").remove();
-        chart.render();
-    } else if (datashow == 6) {
-        console.log(json[7].title);
-
-        var options = {
-            series: [
-                {
-                    name: json[7].title,
-                    data: json[7].data,
-                },
-            ],
-            chart: {
-                width: 700,
-                type: "radar",
-                toolbar: {
-                    show: true,
-                },
-            },
-            labels: json[7].categories,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 400,
-                        },
-                    },
-                },
-            ],
-            plotOptions: {
-                pie: {
-                    donut: {
-                        labels: {
-                            show: true,
-                            value: {},
-                            total: {
-                                show: true,
-                            },
-                        },
-                    },
-                },
-            },
-            dataLabels: {
-                enabled: true,
-                style: {
-                    fontSize: "12px",
-                    colors: ["#000"],
-                },
-            },
-            colors: json[7].colors,
-            title: {
-                text: json[7].title,
-                align: "center",
-            },
-            legend: {
-                position: "top",
-            },
-            fill: {
-                opacity: 0.2,
-            },
+            // legend: {
+            //     position: "top",
+            // },
         };
 
         var chart = new ApexCharts(document.querySelector("#chart"), options);
